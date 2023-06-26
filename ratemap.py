ba1baa7396cb622d4b0a5aa90fb46c9f020c9f81
MaxEaton <maximiliano.k.eaton@gmail.com>
2023-06-26 10:44:17 -0600

    working simulation

--- ratemap.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append("new/msprime/")
-import msprime
-import numpy as np
-
-ts = msprime.sim_ancestry(2, sequence_length=100, random_seed=1234,
-                                record_provenance=False, num_replicates=1)
-print(ts.__dir__())
-# mts = msprime.sim_mutations(ts, rate=0.01, random_seed=5678)
